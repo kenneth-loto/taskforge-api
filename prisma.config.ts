1ca126068a329a24fc7eb06166a59aac5384e1b5
Loto-Kenneth <116004412+Loto-Kenneth@users.noreply.github.com>
2026-06-30 00:17:35 +0800

    chore: updated prisma config to use dotenv/config and also removed prisma generate in script

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,18 +1,12 @@
 import "dotenv/config";
 import { defineConfig } from "prisma/config";
 
-const { DATABASE_URL } = process.env;
-
-if (!DATABASE_URL) {
-  throw new Error("DATABASE_URL is required in environment");
-}
-
 export default defineConfig({
   schema: "prisma/",
   migrations: {
     path: "prisma/migrations",
   },
   datasource: {
-    url: DATABASE_URL,
+    url: process.env.DATABASE_URL,
   },
 });
